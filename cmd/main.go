27e0ee40df411d15618e31703b205d48e06a36a1
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-07-27 17:29:31 -0700

    implement cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,84 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/stripedpajamas/caesar"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "caesar"
+	app.Usage = "encrypt/decrypt using Caesar cipher"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "encrypt",
+			Aliases: []string{"e"},
+			Usage:   "encrypt text",
+			Action:  encrypt,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "key, k",
+					Value: "m",
+					Usage: "the Caesar key",
+				},
+				cli.StringFlag{
+					Name:  "text, t",
+					Usage: "the text to encrypt",
+				},
+			},
+		},
+		{
+			Name:    "decrypt",
+			Aliases: []string{"d"},
+			Usage:   "decrypt text",
+			Action:  decrypt,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "key, k",
+					Value: "m",
+					Usage: "the Caesar key",
+				},
+				cli.StringFlag{
+					Name:  "text, t",
+					Usage: "the text to encrypt",
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func encrypt(c *cli.Context) error {
+	fmt.Println(caesar.Encrypt(c.String("text"), parseKey(c.String("key"))))
+	return nil
+}
+
+func decrypt(c *cli.Context) error {
+	fmt.Println(caesar.Decrypt(c.String("text"), parseKey(c.String("key"))))
+	return nil
+}
+
+func parseKey(input string) int {
+	if len(input) < 1 {
+		log.Fatal("key must be a letter or a number")
+	}
+	k, err := strconv.Atoi(input)
+	if err == nil {
+		// input is a number
+		return k
+	}
+
+	// input is a letter
+	return int(strings.ToLower(input)[0] - 97)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/stripedpajamas/caesar
 
 go 1.12
+
+require (
+	github.com/google/pprof v0.0.0-20190723021845-34ac40c74b70 // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/urfave/cli v1.20.0
+	golang.org/x/arch v0.0.0-20190312162104-788fe5ffcd8c // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/sys v0.0.0-20190726091711-fc99dfbffb4e // indirect
+	golang.org/x/tools v0.0.0-20190727173135-db2fa46ec33c // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
+)
