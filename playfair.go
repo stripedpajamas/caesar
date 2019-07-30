baab8d17afd0e21ed2534a94188cad717e3bc220
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-07-29 20:46:13 -0700

    Stub out playfair implementation

---
+++ playfair.go
@@ -0,0 +1,13 @@
+package caesar
+
+// PlayfairEncrypt constructs a playfair alphabet from key,
+// and then encrypts plaintext using it
+func PlayfairEncrypt(plaintext string, key string) string {
+	return ""
+}
+
+// PlayfairDecrypt constructs a playfair alphabet from key,
+// and then decrypts ciphertext using it
+func PlayfairDecrypt(ciphertext string, key string) string {
+	return ""
+}
---
+++ playfair_test.go
@@ -0,0 +1,51 @@
+package caesar_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stripedpajamas/caesar"
+)
+
+func TestPlayfairEncrypt(t *testing.T) {
+	// example from https://en.wikipedia.org/wiki/Playfair_cipher#Example
+	testCases := []struct {
+		input    string
+		key      string
+		expected string
+	}{
+		{
+			"Hide the gold in the tree stump",
+			"playfair example",
+			"BMODZBXDNABEKUDMUIXMMOUVIF",
+		},
+	}
+
+	for idx, tc := range testCases {
+		if actual := caesar.PlayfairEncrypt(tc.input, tc.key); actual != tc.expected {
+			fmt.Printf("(en) test %d failed: wanted %s, got %s\n", idx, tc.expected, actual)
+			t.Fail()
+		}
+	}
+}
+
+func TestPlayfairDecrypt(t *testing.T) {
+	testCases := []struct {
+		input    string
+		key      string
+		expected string
+	}{
+		{
+			"BMODZBXDNABEKUDMUIXMMOUVIF",
+			"playfair example",
+			"Hide the gold in the tree stump",
+		},
+	}
+
+	for idx, tc := range testCases {
+		if actual := caesar.PlayfairDecrypt(tc.input, tc.key); actual != tc.expected {
+			fmt.Printf("(de) test %d failed: wanted %s, got %s\n", idx, tc.expected, actual)
+			t.Fail()
+		}
+	}
+}
